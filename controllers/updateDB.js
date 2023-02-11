225879815d73279660e098d7b2a2cd43c3b09a57
Cyril Lazar <klazar@mail.ru>
2023-02-11 18:36:21 +1000

    fix: перевел обработку объектов в отдельный файл

--- controllers/updateDB.js
+++ controllers/updateDB.js
@@ -1,21 +1,19 @@
 const fs = require('fs');
 const path = require('path');
-const User = require('../models/User');
-const Department = require('../models/Department');
-const Spot = require('../models/Spot');
+// const User = require('../models/User');
+// const Department = require('../models/Department');
+// const Spot = require('../models/Spot');
 
 const makeObj = require('../utils/makeObj');
 const unzip = require('../utils/unzip');
 
-const EmptyError = require('../errors/EmptyError');
+// const EmptyError = require('../errors/EmptyError');
 const departments = require('../constants/departments');
+const objectHandle = require('../utils/objectHandle');
 
 async function updateDB(filePath, req, res, next) {
-
   // await unzip(filePath).then(console.log);
   const dirTemp = path.resolve('.temp');
-  let countDep = 0,
-    countUser = 0;
 
   try {
     const files = await unzip(filePath).then((fileArray) =>
@@ -23,34 +21,20 @@ async function updateDB(filePath, req, res, next) {
     );
 
     for (i = 0; i <= 13; i++) {
-      const obj = makeObj(path.join(dirTemp,files[i]));
-      if (obj) {
-        const period = obj.period;
+      const obj = makeObj(path.join(dirTemp, files[i]));
+      objectHandle(obj, departments[i]);
 
-        const dep = await Department.createOrUpdate(departments[i], period);
-        if (!dep) continue; // если dep==underfined -  файл не имеет новой информации, можно его пропустить
-        countDep++;
-        const arrayDepartment = obj.createArray();
-        arrayDepartment.forEach(async (el) => {
-          /* разбираем объект - создаем пользователя если его нет, 
-              и создаем документ спота или изменяем его если он есть
-          */
-          const user = await User.findOrCreate(el.name);
-          countUser++;
-          el.spots.forEach(async (spt) => {
-            await Spot.createOrUpdate(spt, dep, user);
-          });
-        });
-      }
     }
   } catch (err) {
     return next(err);
   } finally {
     fs.rm(dirTemp, { recursive: true, force: true }, next);
-    if(countDep==0&&countUser==0){
-      return res.status(400).send({message:'Ничего нового!'});
-    }
-    return res.send({message:`Обновлено департаментов: ${countDep} Обновлено записей пользователя: ${countUser}`});
+    // if (countDep == 0 && countUser == 0) {
+    //   return res.status(400).send({ message: 'Ничего нового!' });
+    // }
+    // return res.send({
+    //   message: `Обновлено департаментов: ${countDep} Обновлено записей пользователя: ${countUser}`,
+    // });
   }
 }
 
---
+++ utils/objectHandle.js
@@ -0,0 +1,24 @@
+const Department = require('../models/Department');
+const Spot = require('../models/Spot');
+const User = require('../models/User');
+
+module.exports = async (obj, departments) => {
+  if (obj) {
+    const period = obj.period;
+
+    const dep = await Department.createOrUpdate(departments, period);
+    if (!dep) return; // если dep==underfined -  файл не имеет новой информации, можно его пропустить
+
+    const arrayDepartment = obj.createArray();
+    arrayDepartment.forEach(async (el) => {
+      /* разбираем объект - создаем пользователя если его нет, 
+          и создаем документ спота или изменяем его если он есть
+      */
+      const user = await User.findOrCreate(el.name);
+
+      el.spots.forEach(async (spt) => {
+        await Spot.createOrUpdate(spt, dep, user);
+      });
+    });
+  }
+};
